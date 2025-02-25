0579974581bbe56305ed592dd28fe85935255c51
Duda-Santosr <maria.e.santosr13@gmail.com>
2025-02-25 09:47:50 -0300

    commit

--- classe.js
+++ classe.js
@@ -6,12 +6,25 @@ class Aluno{
         this.idade= idade;
     }
     estudar = () => console.log(`${this.nome} está estudando.`)
+    feliz = () => console.log(`Olá, meu nome é ${this.nome} tenho ${this.idade} anos, e estou feliz.`)
 }
 
 const duda = new Aluno("Duda", 16)
-console.log(duda.nome)
+console.log(duda.nome);
 
 // novo objeto
-const ana = new Aluno ("Ana", 17)
+const ana = new Aluno ("Ana", 16)
 console.log(ana.nome)
-ana.estudar()
+ana.estudar();
+
+// novo objeto com nome e idade
+const isabela = new Aluno("Isabela", 17)
+console.log(isabela.nome)
+console.log(isabela.idade)
+
+// novo objeto nome, idade e frase
+const yasmin = new Aluno("Yasmin", 17)
+console.log(yasmin.nome)
+console.log(yasmin.idade)
+yasmin.feliz();
+
